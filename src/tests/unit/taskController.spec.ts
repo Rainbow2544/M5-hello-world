b13e66f36458a92642cb8d8d5865fd8378ea9b63
Rainbow <kenesou@gmail.com>
2023-05-28 21:57:28 +1200

    first commit

---
+++ src/tests/unit/taskController.spec.ts
@@ -0,0 +1,47 @@
+import { getAllTasks,getOneTask } from "../../controllers/taskController"
+import {createRequest,createResponse} from 'node-mocks-http'
+
+describe('getAllTasks', () =>{
+    test('should send all tasks via res', ()=> {
+        //arrange
+        const req = createRequest();
+        const res = createResponse();
+        const expected = [
+            {
+              id: 1,
+              name: 'Mission 01',
+              description: 'Chatbot',
+              isCompleted: false,
+            },
+          ]
+
+        //Act
+        getAllTasks(req, res)
+
+        //Assert
+        expect(res.json()._getData()).toEqual(expected)
+    })
+})
+
+describe('getOneTasks', () =>{
+    test('when id 1 returns the default task', ()=> {
+        //arrange
+        const req = createRequest();
+        const res = createResponse();
+        const expected = {
+              id: 1,
+              name: 'Mission 01',
+              description: 'Chatbot',
+              isCompleted: false,
+        };
+
+        req.body.id = '1' ;
+
+        //Act
+        getOneTask(req, res);
+        
+        //Assert
+        expect(res.statusCode).toBe(200);
+        expect(res.json()._getData()).toEqual(expected);
+    })
+})
