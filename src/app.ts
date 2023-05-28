5e50769090da1a5dbd8f1ac09518f86eff8d8125
Rainbow <kenesou@gmail.com>
2023-05-28 22:29:26 +1200

    delete tests, create docker-compost, install mongo

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import taskRoutes from './routes'
+import mongoose from 'mongoose'
 
+mongoose.connect("mongodb://mongo:27018/Samp_data");
 const app = express()
 app.use(express.json())
 
